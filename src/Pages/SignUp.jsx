c2713f47c3fda720d9b52dd5658c1a79c83b6937
Naveen Kumar <nk24528.nk81@gmail.com>
2024-05-10 12:39:06 +0530

    login bug fixed

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
-import { Box, FormControl, FormLabel, Input, Button, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
-const navigate = useNavigate()
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const navigate = useNavigate();
   const handleSignUp = async (e) => {
     e.preventDefault();
-    const formState = { email, password, username };
+    const formState = { email, password, firstName, lastName };
     try {
       const register = await axios.post(
         `https://asos-app-backend.onrender.com/users/signUp`,
@@ -18,19 +26,19 @@ const navigate = useNavigate()
       );
 
       console.log(register);
-      navigate("/login")
+      navigate("/login");
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    
     <Box maxW="md" mx="auto" mt={8} p={10} borderWidth="1px" borderRadius="lg">
-      <Heading textAlign={"center"} textDecoration={"underline"}>Sign Up</Heading>
+      <Heading textAlign={"center"} textDecoration={"underline"}>
+        Sign Up
+      </Heading>
       <br />
-      <form  onSubmit={handleSignUp}>
-        
+      <form onSubmit={handleSignUp}>
         <FormControl id="email" mb={4}>
           <FormLabel>Email Address</FormLabel>
           <Input
@@ -41,10 +49,18 @@ const navigate = useNavigate()
         </FormControl>
 
         <FormControl id="username" mb={4}>
-          <FormLabel>Username</FormLabel>
+          <FormLabel>firstName</FormLabel>
           <Input
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </FormControl>
+
+        <FormControl id="username" mb={4}>
+          <FormLabel>lastName</FormLabel>
+          <Input
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
           />
         </FormControl>
 
@@ -57,7 +73,7 @@ const navigate = useNavigate()
           />
         </FormControl>
 
-        <Button type="submit" colorScheme="blue" >
+        <Button type="submit" colorScheme="blue">
           Sign Up
         </Button>
       </form>
